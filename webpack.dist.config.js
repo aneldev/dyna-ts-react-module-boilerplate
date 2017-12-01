9f390f2a2d91b3e7bf5f46742d331f2a05d16202
Dennis Teo <dennis@anel.co>
2017-12-01 18:14:36 +0100

    remove useless scrips, clear and build

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -10,7 +10,7 @@ const plugins = require('./webpack.plugins');
 
 const config = {
   entry: [
-    // do not user babel-polyfill here, the application should load polyfills!
+    // do not load babel-polyfill here, the application should load the polyfills!
     // the entry application code
     path.resolve(__dirname, 'src/index.tsx')
   ],
