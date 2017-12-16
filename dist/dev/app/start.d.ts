08d4bb3c6caf2d5e164d251fe1fc71a6555d59fe
Dennis Teo <dennis@anel.co>
2017-12-16 16:20:40 +0100

    support enzyme.mount

--- dist/dev/app/start.d.ts
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-import "./font-awesome.less";
-import "./start.less";
-export default class StartApp extends React.Component<any, any> {
-    render(): JSX.Element;
-}
