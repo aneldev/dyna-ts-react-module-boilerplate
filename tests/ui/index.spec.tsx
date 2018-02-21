8bf3d8398c99c38b24c6d6fc52a025932dc3e89c
Dennis Teo <dennis@anel.co>
2018-02-21 07:18:58 +0100

    test ts globals

--- tests/ui/index.spec.tsx
+++ tests/ui/index.spec.tsx
@@ -1,3 +1,5 @@
+declare let global: any, jasmine: any, describe: any, clearTest: any, it: any, expect: any;
+
 import { configure } from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
