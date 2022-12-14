59fc244a7800f00259426126e0978eccd3ddfa71
Dennis <dennisat@users.noreply.github.com>
2022-12-14 08:43:19 +0100

    Upgrade: Boilerplate (#45)
    Co-authored-by: dennisat <dennis@anel.co>

--- tests/ui/Button.spec.tsx
+++ tests/ui/Button.spec.tsx
@@ -1,18 +1,28 @@
-import {configure} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import * as React from "react";
+import * as enzyme from "enzyme";
 
-import * as React from 'react';
-import * as enzyme from 'enzyme';
+// @ts-ignore
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+enzyme.configure({ adapter: new Adapter() });
 
 import {Button} from '../../src/Button';
-
-configure({adapter: new Adapter()});
 
 describe('Button', () => {
   let wrapper;
 
-  test('with minimal props', () => {
+  test('swallow with minimal props', () => {
     wrapper = enzyme.shallow(
+      (
+        <Button onClick={() => console.log('I am clicked')}>My button</Button>
+      ),
+      {},
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  });
+  test('mount with minimal props', () => {
+    wrapper = enzyme.mount(
       (
         <Button onClick={() => console.log('I am clicked')}>My button</Button>
       ),
