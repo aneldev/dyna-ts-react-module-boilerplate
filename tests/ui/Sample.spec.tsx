fdce875cf611e4e776fd78f6acb016cb032d2841
dennisat <dennis@anel.co>
2024-08-27 10:37:38 +0200

    Dev: In progress...

--- tests/ui/Sample.spec.tsx
+++ tests/ui/Sample.spec.tsx
@@ -1,26 +1,17 @@
-import {configure} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-import * as React from 'react';
-import * as enzyme from 'enzyme';
-
-configure({adapter: new Adapter()});
+import { render } from "@testing-library/react";
 
 describe('Sample UI test', () => {
   let wrapper;
 
   test('with minimal props', () => {
-    wrapper = enzyme.shallow(
-      (
+    expect(
+      render(
         <div
           onClick={() => console.log('I am clicked')}
         >
           Demo empty div
         </div>
-      ),
-      {},
-    );
-
-    expect(wrapper).toMatchSnapshot();
+      )
+    ).toMatchSnapshot();
   });
 });
