6ec27ec8a4ab19cedf5e2d3bd3191434fbf62dc7
dennisat <dennis@anel.co>
2024-08-27 10:37:38 +0200

    Dev: In progress...

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,7 +3,6 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const loaders = require('./webpack.loaders');
 const plugins = require('./webpack.plugins');
