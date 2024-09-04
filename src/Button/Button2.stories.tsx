812046016603f72be1c1236146a444c630ecd714
dennisat <dennis@anel.co>
2024-09-04 08:11:50 +0200

    Linting

--- src/Button/Button2.stories.tsx
+++ src/Button/Button2.stories.tsx
@@ -25,8 +25,4 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Run: Story = {
-  args: {
-    children: 'Click me',
-  },
-};
+export const Run: Story = {args: {children: 'Click me'}};
