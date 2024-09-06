0ff51eb423b12927ec1956928cb22537f72411f1
dennisat <dennis@anel.co>
2024-09-06 17:45:55 +0200

    Fix: Story VS style loaders

--- stories/boilerplate/css-loaders/css-loaders.stories.tsx
+++ stories/boilerplate/css-loaders/css-loaders.stories.tsx
@@ -1,10 +1,12 @@
 import {Meta} from '@storybook/react/types-6-0';
 
-import "./css-loaders.css";
+import "./css-loaders-pure.css";
+
 import "./css-loaders.less";
 import * as lessStyles from './css-loaders.module.less';
-// import "./css-loaders.scss";
-// import * as sassStyles from './css-loaders.module.scss';
+
+import "./css-loaders.scss";
+import * as sassStyles from './css-loaders.module.scss';
 
 const meta: Meta = {
   title: 'Boilerplate',
@@ -19,11 +21,11 @@ export const CSSLoaders = () => (
   <div style={{backgroundColor: "white"}}>
     <h1>CSS loaders</h1>
 
-    <h3 className="intro-css-loaders__label--css">CSS loader, should be gray</h3>
+    <h3 className="intro-css-loaders__label--css">CSS loader, should be blue</h3>
     <h3 className="intro-css-loaders__label--less">LESS loader, should be purple</h3>
-    <h3 className={lessStyles.label}>LESS module loader, should be blueviolet</h3>
-    {/*<h3 className="intro-css-loaders__label--scss">SCSS loader, should be pink</h3>*/}
-    {/*<h3 className={sassStyles.label}>SCSS module loader, should be deeppink</h3>*/}
+    <h3 className={lessStyles.label}>LESS module loader, should be violet</h3>
+    <h3 className="intro-css-loaders__label--scss">SCSS loader, should be pink</h3>
+    <h3 className={sassStyles.label}>SCSS module loader, should be deeppink</h3>
   </div>
 );
 
