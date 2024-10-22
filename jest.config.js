3c12e6ad19f39dd463b31ba9447e9c99b01bd851
dennisat <dennis@anel.co>
2024-10-22 17:29:37 +0200

    Fix: Coverage error for index.ts files (exclude them)

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,15 @@ module.exports = {
   collectCoverage: false,
   testEnvironment: "jsdom",
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.jest.json",
+        diagnostics: {
+          warnOnly: true,  // Set to true to avoid breaking the test run, but still see the diagnostics
+        },
+      },
+    ],
   },
   transformIgnorePatterns: ["/node_modules/", "\\.pnp\\.[^\\/]+$"],
   testRegex: "(src)/.*\\.(test|spec)\\.(ts|tsx)$",
@@ -17,5 +25,9 @@ module.exports = {
     "src/**/*.{ts,tsx}",
     "!src/@types",
     "!src/**/*.d.ts",
+    "!src/index.ts",
+    "!src/index.tsx",
+    "!src/*/index.ts",
+    "!src/*/index.tsx"
   ],
 };
